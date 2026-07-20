7dc3fb6e549cb40ef59664afc8fc265484e134df
landkirk <landonaddison912@gmail.com>
2026-07-20 13:33:44 -0400

    Release v3.1.0

--- src/util/screens.ts
+++ src/util/screens.ts
@@ -12,9 +12,9 @@ export const SCREENS: OnboardingScreen[] = [
     bodyFile: 'ui/screens/bing-warning-v1.html',
   },
   {
-    id: 'changelog-3.0.2',
-    title: "What's New in 3.0.2",
-    bodyFile: 'ui/screens/changelog-3.0.2.html',
+    id: 'changelog-3.1.0',
+    title: "What's New in 3.1.0",
+    bodyFile: 'ui/screens/changelog-3.1.0.html',
   },
 ];
 
